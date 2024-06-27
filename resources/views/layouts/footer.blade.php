df2a2770e76edbc17a293efa558b4d245476b5e0
gun15992 <gun15992@gmail.com>
2024-06-27 16:21:11 +0700

    Project Version 0.0.1

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+    </main>
+    <footer>
+        <!-- Your Content Code Here -->
+    </footer>
+    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.js') }}"></script>
+</body>
+</html>
