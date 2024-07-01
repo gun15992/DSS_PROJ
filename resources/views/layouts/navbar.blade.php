c07fa52424441d385f00ce68780421befbf80258
gun15992 <gun15992@gmail.com>
2024-07-01 08:59:05 +0700

    Project V 0.0.2 - Edit Navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -9,7 +9,7 @@
                 <div class="collapse navbar-collapse" id="navbarText">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a href="/" class="nav-link fw-bold active" style="margin-right: 10px; margin-left: 15px;">หน้าหลัก</a>
+                            <a href="/" class="nav-link fw-bold active" style="margin-right: 10px;">หน้าหลัก</a>
                         </li>
                         <li class="nav-item">
                             <a href="/dashboard" class="nav-link fw-bold" style="margin-right: 10px;">สรุปภาพรวม</a>
@@ -18,7 +18,7 @@
                             <a href="/type" class="nav-link fw-bold" style="margin-right: 10px;">ประเภทครุภัณฑ์</a>
                         </li>
                         <li class="nav-item">
-                            <div class="dropdown show">
+                            <div class="dropdown show d-flex">
                                 <a href="#" class="btn dropdown-toggle nav-link fw-bold" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="margin-right: 20px;">ค้นหาครุภัณฑ์</a>
                                 <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
                                     <a class="dropdown-item" href="/type-search/name">ค้นหาตามชื่อครุภัณฑ์</a>
@@ -30,14 +30,14 @@
                         </li>
                         <form class="d-flex">
                             <input class="form-control me-2" type="search" placeholder="ค้นหาครุภัณฑ์" aria-label="Search" style="margin-right: 5px;">
-                            <button class="btn btn-me btn-light" type="submit" style="margin-right: 20px;">
+                            <button class="btn btn-me btn-light" type="submit" style="margin-right: 20p">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                                     <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
                                 </svg>
                                 ค้นหา
                             </button>
                         </form>
-                        <li class="nav-item ">
+                        <li class="nav-item">
                             <a href="/signin" class="btn btn-primary btn-me active" role="button" aria-pressed="true">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
                                     <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
